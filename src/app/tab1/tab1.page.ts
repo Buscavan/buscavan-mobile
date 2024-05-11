c252e81504165a911a697e807b9d7e019e72566c
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-05-11 01:44:57 -0300

    Header and landing page new design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
 
+  public scrolled(event: any) {
+    const topPosition = event.detail.scrollTop;
+    const tabBar: any = document.querySelector('ion-tab-bar');
+
+    tabBar.style.opacity = topPosition > 20 ? 1 : 0;
+  }
+
+  ngOnInit(): void {
+    const tabBar: any = document.querySelector('ion-tab-bar');
+    tabBar.style.opacity = 0;
+    setTimeout(() => tabBar.style.transition = 'opacity .2s ease-out', 300);
+  }
 }
