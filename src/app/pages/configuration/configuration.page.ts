5456134a2d44891c8db4ccf95a2f28f7d87b0051
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-06-22 08:45:41 -0300

    Configurações mostrando dados corretos

--- src/app/pages/configuration/configuration.page.ts
+++ src/app/pages/configuration/configuration.page.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputComponent } from 'src/app/components/input/input.component';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.page.html',
   styleUrls: ['./configuration.page.scss'],
 })
-export class ConfigurationPage {
+export class ConfigurationPage implements OnInit {
 
   inputs: InputComponent[] = [
     {
@@ -41,6 +43,42 @@ export class ConfigurationPage {
     },
   ];
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  async ngOnInit() {
+    const user: User = await this.authService.getUser();
+    this.inputs = [
+      {
+        type: 'text',
+        placeholder: user.name || '',
+        label: 'Nome',
+        size: 12,
+        disabled: false,
+        ngOnInit: () => {},
+        turnDisabled: () => {},
+        value: '',
+      },
+      {
+        type: 'text',
+        placeholder: user.email || '',
+        label: 'E-mail',
+        size: 12,
+        disabled: false,
+        ngOnInit: () => {},
+        turnDisabled: () => {},
+        value: '',
+      },
+      {
+        type: 'text',
+        placeholder: user.cpf || '',
+        label: 'CPF',
+        size: 12,
+        disabled: false,
+        ngOnInit: () => {},
+        turnDisabled: () => {},
+        value: '',
+      },
+    ];
+  }
 
 }
